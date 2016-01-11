956e01db2700d78d542d965e73af742838745853
Casey Greene <csgreene@upenn.edu>
2016-01-11 16:38:28 -0500

    comment on directories.R should specify how to point elsewhere

--- directories.R
+++ directories.R
@@ -1,4 +1,4 @@
-# Change somepathto below.
+# Change paths below if you want to read from/write to another directory.
 initial_data = "data/"
 normalized_data = "normalized_data/"
 output = "output/"
