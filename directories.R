ce5fbcc31d0c139e2f4795002c1b61a8b20b9000
Casey Greene <csgreene@upenn.edu>
2016-01-11 16:32:09 -0500

    make directories relative to make code more portable

--- directories.R
+++ directories.R
@@ -1,4 +1,4 @@
 # Change somepathto below.
-initial_data = "/global/scratch/jefft/data/"
-normalized_data = "/global/scratch/jefft/normalized_data/"
-output = "/global/scratch/jefft/output/"
+initial_data = "data/"
+normalized_data = "normalized_data/"
+output = "output/"
